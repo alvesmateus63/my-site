a5e742bb2296c7065abc7aaeb3179052aa9dc10b
Mateus Alves <alvesmateus.ps@gmail.com>
2024-01-28 23:33:43 -0300

    refactor: extract the sections title into a component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SectionTitleComponent } from '../shared/section-title/section-title.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [SectionTitleComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
